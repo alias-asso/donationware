04775b6802d022e0026bcfde13dd71fbd8c5bd07
Ionys <9364594+Ionys320@users.noreply.github.com>
2026-04-22 17:19:52 +0200

    fix(anaconda_report.sh): update MAC address retrieval method for accuracy

--- pxe/fedora/anaconda_report.sh
+++ pxe/fedora/anaconda_report.sh
@@ -2,7 +2,7 @@
 url="http://10.0.0.1:5000/computer/"
 
 send() {
-  mac_address=$(ip r g 10.0.0.1 | cut -d ' ' -f2) # Get the MAC address of the interface used to reach the server
+  mac_address=$(ip n s 10.0.0.1 | awk '{print $5}') # Get the MAC address of the interface used to reach the server
   
   url_with_mac="${url}${mac_address}"
 
